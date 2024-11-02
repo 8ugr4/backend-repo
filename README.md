a6d6e4c0a1d0a16af26e1eed4dec04c30886438b
8ugr4 <karadumanbugra@hotmail.com>
2024-11-02 16:45:03 +0100

    chore: fix .idea and add migration sql files.

--- README.md
+++ README.md
@@ -2,7 +2,21 @@
 my learning space for go + postgres to build some app. this description will update itself over time.
 
 ## development tracking
+
+### Docker 
 * postgres on docker created.
+* * docker ps -a : list all containers.
 * * docker run --name <container-name> -p <PORT:PORT> -e POSTGRES_USER=<user-name> -e POSTGRES_PASSWORD=<pass> -d postgres
 * * docker exec -it <container-name> psql -U <user-name>
-* * docker logs postgres-bank
+* * or, docker exec -it <c-name> /bin/sh
+* * docker logs <container-name>
+
+### Migration
+* Up and Down migrations are used to update from old to new database or other way around.
+* i.e.: old db --> (migrate up) --> x.up.sql --> new db
+* i.e.: new db --> (migrate down) --> x.down.sql --> old db
+
+### Postgres
+* createdb --username=<> --owner=<> <database_name>
+* or outside docker, docker exec -it <c-name> createdb --username...
+* 
---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,42 @@
+CREATE TABLE "accounts" (
+                            "id" int PRIMARY KEY,
+                            "owner" varchar NOT NULL,
+                            "balance" bigint NOT NULL,
+                            "currency" varchar NOT NULL,
+                            "created_at" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+CREATE TABLE "entries" (
+                           "id" bigserial PRIMARY KEY,
+                           "account_id" bigint NOT NULL,
+                           "amount" bigint NOT NULL,
+                           "created_at" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+CREATE TABLE "transactions" (
+                                "id" bigserial PRIMARY KEY,
+                                "amount" bigint NOT NULL,
+                                "from_account_id" bigint NOT NULL,
+                                "to_accound_id" bigint NOT NULL,
+                                "created_at" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transactions" ("from_account_id");
+
+CREATE INDEX ON "transactions" ("to_accound_id");
+
+CREATE INDEX ON "transactions" ("from_account_id", "to_accound_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
+
+COMMENT ON COLUMN "transactions"."amount" IS 'must be positive';
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transactions" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transactions" ADD FOREIGN KEY ("to_accound_id") REFERENCES "accounts" ("id");
